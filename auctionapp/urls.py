2fc03735901ac9d46896f41ff2cff5b947b71a01
KTheProgrammer <57043715+KTheProgrammer@users.noreply.github.com>
2021-07-28 19:57:16 -0500

    Update urls.py

--- auctionapp/urls.py
+++ auctionapp/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('register', views.register),
+    path('register/', views.register),
     path('create_account', views.create_account),
     path('login', views.login),
-    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('marketplace/', views.marketplace),
+    path('create', views.create),
+    path('<int:item_id>/delete', views.delete),
 ]
